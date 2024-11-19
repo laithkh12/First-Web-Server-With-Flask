9a127697a4422a4dc57a7cddfd21b62024270615
laithkh12 <168769648+laithkh12@users.noreply.github.com>
2024-11-19 19:46:26 +0200

    Update hello.py

--- First Web Server With Flask/hello.py
+++ First Web Server With Flask/hello.py
@@ -2,16 +2,37 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Define the decorators
+def make_bold(f):
+    def wrapper():
+        return f"<b>{f()}</b>"
+    return wrapper
+
+def make_emphasis(f):
+    def wrapper():
+        return f"<em>{f()}</em>"
+    return wrapper
+
+def make_underlined(f):
+    def wrapper():
+        return f"<u>{f()}</u>"
+    return wrapper
+
 @app.route('/')
 def hello_world():
-    return "Hello World!"
+    return ("<h1 style='text-align: center'>Hello World!</h1>"
+            "<p>This is a paragraph</p>")
 
 @app.route('/bye')
+@make_bold
+@make_emphasis
+@make_underlined
 def say_bye():
     return "Bye!"
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/username/<name>/<int:number>')
+def greet(name, number):
+    return f"Hello there {name}, you are {number} years old."
 
-# $env:FLASK_APP = "hello.py"
-# python -m flask run
+if __name__ == '__main__':
+    app.run(debug=True)
